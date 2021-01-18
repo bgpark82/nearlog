a3573c398644d0d1e1b8af37efffa59ec6671e8c
bgpark <bgpark@citus.co.kr>
2021-01-18 17:13:03 +0900

    feat: 멀티 프로젝트 설정

---
+++ nl-admin/src/test/java/com/bgpark/nearlog/NearlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bgpark.nearlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NearlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
